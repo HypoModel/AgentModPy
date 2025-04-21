77128f71fd6385df2aba13d3665888c5e4261e76
Duncan MacGregor <doctorafternoon@gmail.com>
2025-04-21 20:08:26 +0800

    Initial commit

---
+++ HypoModPython.py
@@ -0,0 +1,29 @@
+## HypoModPython
+##
+## Started 5/11/18
+## Continued 24/8/22
+##
+## Duncan MacGregor
+##
+## AgentModPy
+## Started 21/5/25 in ZJE Haining
+
+
+import wx
+from HypoModPy.hypomain import *
+
+
+
+app = wx.App(False)
+pos = wx.DefaultPosition
+size = wx.Size(400, 500)
+mainpath = ""
+respath = ""
+modname = "Agent"
+mainwin = HypoMain("HypoMod", pos, size, respath, mainpath, modname)
+mainwin.Show()
+mainwin.SetFocus()
+app.MainLoop()
+
+
+
